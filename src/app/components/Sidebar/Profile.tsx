8640525b01f97b54f805260a6f25993d286e17e9
mathzsl <maths.soares99@gmail.com>
2023-08-02 21:49:53 -0300

    feat(sidebar): Add user profile

---
+++ src/app/components/Sidebar/Profile.tsx
@@ -0,0 +1,30 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="flex items-center gap-3">
+      <img
+        src="https://github.com/mathzsl.png "
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+
+      <div className="flex flex-col truncate">
+        <span className="truncate text-sm/5 font-semibold text-zinc-700 ">
+          Matheus Soares
+        </span>
+        <span className="truncate text-sm/5 text-zinc-500">
+          maths.soarsdfsdfsdfsdfsdes99@gmail.com
+        </span>
+      </div>
+
+      <button
+        type="button"
+        title="Logout"
+        className="ml-auto rounded-md p-2 hover:bg-zinc-50"
+      >
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -3,10 +3,11 @@ import { Logo } from './Logo'
 import { MainNavigation } from './MainNavigation'
 import { NavItem } from './MainNavigation/NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
+import { Profile } from './Profile'
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col gap-6 border-r border-zinc-300 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="mx-1 flex items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -26,6 +27,10 @@ export function Sidebar() {
         </nav>
 
         <UsedSpaceWidget />
+
+        <div className="h-px bg-zinc-200" />
+
+        <Profile />
       </div>
     </aside>
   )
