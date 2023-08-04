3f24eed442295d23df5393ab15fbb8106681a3e7
mathzsl <maths.soares99@gmail.com>
2023-08-04 00:57:01 -0300

    feat: creating file input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Mail, UploadCloud, User } from 'lucide-react'
+import { InputControl, InputPrefix, InputRoot } from './components/Input'
 import { SettingsTab } from './components/SettingsTabs'
 
 export default function Home() {
@@ -33,8 +35,169 @@ export default function Home() {
           </div>
         </div>
 
-        <form id="settings" action="" className="mt-6 flex w-full flex-col">
-          <form action="">form</form>
+        <form
+          id="settings"
+          action=""
+          className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200"
+        >
+          <div className="grid grid-cols-form gap-3">
+            <label
+              htmlFor="firstName"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Name
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <InputRoot>
+                <InputControl
+                  id="firstName"
+                  type="text"
+                  defaultValue="Matheus"
+                />
+              </InputRoot>
+
+              <InputRoot>
+                <InputControl type="text" defaultValue="Soares" />
+              </InputRoot>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Email
+            </label>
+
+            <InputRoot>
+              <InputPrefix>
+                <Mail className="h-5 w-5 text-zinc-500" />
+              </InputPrefix>
+              <InputControl
+                id="email"
+                type="email"
+                defaultValue="maths.soares99@gmail.com"
+              />
+            </InputRoot>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="photo"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Your photo
+              <span className="mt-0.5  block text-sm font-normal text-zinc-500 ">
+                This will be displayed on your profile.
+              </span>
+            </label>
+
+            <div className="flex items-start gap-5">
+              <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
+                <User className="h-8 w-8 text-violet-500" />
+              </div>
+
+              <label
+                htmlFor="photo"
+                className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-200 px-6 py-4 text-center text-sm text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
+              >
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                  <UploadCloud className="h-5 w-5  text-zinc-600 group-hover:text-violet-600" />
+                </div>
+
+                <div className="flex flex-col items-center gap-1">
+                  <span className="text-sm">
+                    <span className="font-semibold text-violet-700">
+                      Click to upload{' '}
+                    </span>
+                    or drog and drop
+                  </span>
+                  <span className="text-xs">
+                    SVG, PNG, JPG or GIF (max. 800x400px)
+                  </span>
+                </div>
+              </label>
+
+              <input type="file" className="sr-only" id="photo" />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="role" className="text-sm font-medium text-zinc-700">
+              Role
+            </label>
+
+            <InputRoot>
+              <InputControl
+                id="role"
+                type="text"
+                defaultValue="Frontend developer"
+              />
+            </InputRoot>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="country"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Country
+            </label>
+
+            <div></div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="timezone"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Timezone
+            </label>
+
+            <div></div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="bio" className="text-sm font-medium text-zinc-700">
+              Bio
+              <span className="mt-0.5  block text-sm font-normal text-zinc-500 ">
+                Write a short introduction.
+              </span>
+            </label>
+
+            <div></div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="projects"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Portfolio projects
+              <span className="mt-0.5  block text-sm font-normal text-zinc-500 ">
+                Share a few snippets of your work.
+              </span>
+            </label>
+
+            <div></div>
+          </div>
+
+          <div className="flex items-center justify-end gap-3 pt-5">
+            <button
+              type="button"
+              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-500 shadow-sm hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              id="settings"
+              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
         </form>
       </section>
     </>
