7ffdaf9e6d4fbb1fdb1cda5e7cacbda38e4bbab9
mathzsl <maths.soares99@gmail.com>
2023-08-20 19:56:28 -0300

    feat: sidebar dark theme

--- src/app/components/Sidebar/Profile.tsx
+++ src/app/components/Sidebar/Profile.tsx
@@ -11,10 +11,10 @@ export function Profile() {
       />
 
       <div className="flex flex-col truncate">
-        <span className="truncate text-sm/5 font-semibold text-zinc-700 ">
+        <span className="truncate text-sm/5 font-semibold text-zinc-700 dark:text-zinc-100 ">
           Matheus Soares
         </span>
-        <span className="truncate text-sm/5 text-zinc-500">
+        <span className="truncate text-sm/5 text-zinc-500 dark:text-zinc-400">
           maths.soares99@gmail.com
         </span>
       </div>
